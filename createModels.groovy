001b8776def19af1b03f109d19adfc9d833e66dc
Egon Willighagen <egon.willighagen@gmail.com>
2021-11-26 08:33:04 +0100

    Sanatized the Turtle output

--- createModels.groovy
+++ createModels.groovy
@@ -9,13 +9,14 @@ def data_iterator = parseCsv(csv_content, separator: ',', readFirstLine: true)
 println "PREFIX sbd:   <https://www.sbd4nano.eu/rdf/#>"
 println "PREFIX foaf:  <http://xmlns.com/foaf/0.1/>"
 println "PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>"
+println "PREFIX dc:    <http://purl.org/dc/elements/1.1/>"
 println ""
 
 for (line in data_iterator) {
   println "<${line[0]}> a sbd:Model ;"
   println "  rdfs:label \"${line[1]}\"@en ;"
   println "  dc:source <https://h2020-sbd4nano.github.io/sbd-data-aopwiki/> ;"
-  description = line[2].replace("\n"," ").trim()
+  description = line[2].replace("\n"," ").replace("\"", "\\\"").trim()
   println "  dc:description \"${description}\"@en ;"
   println "  foaf:page <${line[0]}> ."
   println ""
